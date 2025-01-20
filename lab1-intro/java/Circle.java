7a98b3b2a5a9b2ffaed4a22023b32b0288d9b98e
Min <118368464+mkktheminkhant@users.noreply.github.com>
2025-01-20 15:57:53 +0000

    Update Circle.java

--- lab1-intro/java/Circle.java
+++ lab1-intro/java/Circle.java
@@ -1,5 +1,5 @@
 class Circle {
-
+//Edit made by Min Khant Kyaw, 230415649
   public static void main(String[] args) {
     int size = 15; // Integer.parseInt(args[0]);
     Circle c = new Circle(size);
